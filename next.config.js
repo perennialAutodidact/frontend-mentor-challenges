da93f3790bc236a655c04db941bfa2b406f66d96
KG <keegood8@gmail.com>
2022-09-06 08:54:01 -0700

    added nextjs-routes

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
-const path = require('path')
+const path = require("path");
+const { withRoutes } = require("nextjs-routes/next-config.cjs");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   sassOptions: {
-    includePaths: [
-      path.join(__dirname, "styles"),
-    ],
-  }
+    includePaths: [path.join(__dirname, "styles")],
+  },
 };
 
-module.exports = nextConfig;
+module.exports = withRoutes(nextConfig);
