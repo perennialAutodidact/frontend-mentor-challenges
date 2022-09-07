f20196374de4f89cabe7c0011d5e2cbd5c9a6e72
keegangood <keegood8@gmail.com>
2022-09-07 14:15:14 -0700

    moved mobile nav logic to its own component

--- components/IntroSectionWithDropDownNav/Navbar/index.tsx
+++ components/IntroSectionWithDropDownNav/Navbar/index.tsx
@@ -2,20 +2,15 @@ import React, { useState } from "react";
 import styles from "styles/pages/IntroSectionWithDropdownNav/IntroSectionWithDropdownNav.module.scss";
 import Logo from "./Logo";
 import NavItems from "./NavItems";
-import HamburgerIcon from "./HamburgerIcon";
+import HamburgerIcon from "./MobileNav/HamburgerIcon";
+import MobileNav from "./MobileNav";
+
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const toggleMobileNav = (e: React.MouseEvent) => {
-    setIsOpen((isOpen) => !isOpen);
-  };
-
   return (
-    <nav className={styles.navbar}>
+    <nav className={styles.navbarDesktop}>
       <Logo />
       <NavItems />
-      {isOpen.toString()}
-      <HamburgerIcon toggleMobileNav={toggleMobileNav} />
     </nav>
   );
 };
