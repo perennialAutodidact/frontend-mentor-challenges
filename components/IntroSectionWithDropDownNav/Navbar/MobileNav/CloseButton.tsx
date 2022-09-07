337e208361b4b54eab61c363fc8de6f1eda5a5a4
keegangood <keegood8@gmail.com>
2022-09-07 14:15:27 -0700

    added onclick to close button

--- components/IntroSectionWithDropDownNav/Navbar/MobileNav/CloseButton.tsx
+++ components/IntroSectionWithDropDownNav/Navbar/MobileNav/CloseButton.tsx
@@ -4,20 +4,22 @@ import styles from "styles/pages/IntroSectionWithDropdownNav/IntroSectionWithDro
 import closeButtonIcon from "components/IntroSectionWithDropDownNav/starterCode/images/icon-close-menu.svg";
 
 interface CloseButtonProps {
-  toggleMobileNav: undefined;
+  toggleMobileNav: React.MouseEventHandler;
 }
 
 const CloseButton = ({ toggleMobileNav }: CloseButtonProps) => {
   const { src, height, width, blurDataURL } = closeButtonIcon;
   return (
     <div className={styles.closeNavButton}>
-      <Image
-        src={src}
-        alt="Close Menu Button"
-        height={height}
-        width={width}
-        blurDataURL={blurDataURL}
-      />
+      <div onClick={toggleMobileNav}>
+        <Image
+          src={src}
+          alt="Close Menu Button"
+          height={height}
+          width={width}
+          blurDataURL={blurDataURL}
+        />
+      </div>
     </div>
   );
 };
